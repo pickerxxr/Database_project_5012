dc1a323e4f29ccdaac47fdae7135b63126fc02b3
pickerxxr <18307100052@fudan.edu.cn>
2020-05-15 18:48:55 +0800

    test

---
+++ insert_players.py
@@ -0,0 +1,12 @@
+import pyodbc
+conn = pyodbc.connect('DRIVER={SQL Server Native Client 10.0};SERVER=localhost;DATABASE = '
+                      'Book_reader_db_liuyouzhe;InitialCatalog=dbo;UID=sa;PWD=123456')
+
+cur = conn.cursor()
+if not cur:
+    raise(NameError, '数据库连接失败')
+
+
+cur.execute("INSERT INTO NBA_players_test (01)")
+test_result = cur.execute("SELECT * FROM NBA_players_test").fetchall()
+print(test_result)
