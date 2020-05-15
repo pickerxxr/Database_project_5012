7097e1c9d2c1ff29b11c989e6efe50286a7ec1e8
pickerxxr <18307100052@fudan.edu.cn>
2020-05-15 16:27:50 +0800

    test

--- trySql.py
+++ trySql.py
@@ -1,8 +1,11 @@
 import pyodbc
-conn = pyodbc.connect('DRIVER={SQL Server Native Client 10.0};SERVER=localhost;DATABASE = Book_reader_db_liuyouzhe;InitialCatalog=dbo;UID=sa;PWD=***')
+conn = pyodbc.connect('DRIVER={SQL Server Native Client 10.0};SERVER=localhost;DATABASE = '
+                      'Book_reader_db_liuyouzhe;InitialCatalog=dbo;UID=sa;PWD=123456')
 
 cur = conn.cursor()
 if not cur:
     raise(NameError, '数据库连接失败')
-re = cur.execute("SELECT 书号 FROM books_liuyouzhe").fetchall()
-print(re)
+cur.execute("CREATE TABLE NBA_players_test(player_id int IDENTITY NOT NULL)")
+
+test_result = cur.execute("SELECT * FROM NBA_players_test").fetchall()
+print(test_result)
