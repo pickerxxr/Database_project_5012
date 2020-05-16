34de7bcdd230162efaec4f4e9feed484d72ef8e5
pickerxxr <18307100052@fudan.edu.cn>
2020-05-16 20:09:38 +0800

    add button function.

--- venv/index.py
+++ venv/index.py
@@ -9,14 +9,46 @@ import sys
 # 实现 ui和Logic的分离
 ui, _ = loadUiType('main.ui')
 
+
 class MainApp(QMainWindow, ui):
 
     # 定义构造方法
     def __init__(self):
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.handle_ui_change()
 
+    # UI 的变化处理
+    def handle_ui_change(self):
+        self.player_button.clicked.connect(self.open_player_tab)
+        self.team_button.clicked.connect(self.open_team_tab)
+        self.games_button.clicked.connect(self.open_games_tab)
+        self.player_compare_button.clicked.connect(self.open_compare_tab)
+        self.heat_map_button.clicked.connect(self.open_heat_tab)
+        self.line_button.clicked.connect(self.open_line_tab)
 
+    # 处理所有button的消息和槽的通信
+    def handle_buttons(self):
+        pass
+
+    # 选项卡的联动
+    def open_player_tab(self):
+        self.tabWidget_allfunc.setCurrentIndex(0)
+
+    def open_team_tab(self):
+        self.tabWidget_allfunc.setCurrentIndex(1)
+
+    def open_games_tab(self):
+        self.tabWidget_allfunc.setCurrentIndex(2)
+
+    def open_compare_tab(self):
+        self.tabWidget_allfunc.setCurrentIndex(3)
+
+    def open_heat_tab(self):
+        self.tabWidget_allfunc.setCurrentIndex(4)
+
+    def open_line_tab(self):
+        self.tabWidget_allfunc.setCurrentIndex(5)
 
 
 def main():
@@ -24,6 +56,11 @@ def main():
     window = MainApp()
     window.show()
     app.exec_()
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
